8188e56f2f6ca60a2839c3b588bcf9d796919a16
LopezTw <caiohg_94@hotmail.com>
2023-08-16 14:37:13 -0300

    Entidades Atividade e Categoria CRIADOS

---
+++ src/main/java/com/devsuperiorcourse/dseventos/entities/Atividade.java
@@ -0,0 +1,78 @@
+package com.devsuperiorcourse.dseventos.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String descricao;
+	private Double preco;
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+	
+	public Atividade() {}
+
+	public Atividade(Long id, String name, String descricao, Double preco, Categoria categoria) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
---
+++ src/test/java/com/devsuperiorcourse/dseventos/DseventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorcourse.dseventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DseventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
